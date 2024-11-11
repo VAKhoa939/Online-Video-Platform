d14b638444c9ab8851a1fc9f5bdd55fae62ef409
Kaos <21110046@student.hcmute.edu.vn>
2024-11-12 00:41:45 +0700

    fix index.ts in backend 1st time

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
 
 const app = express();
@@ -8,6 +9,10 @@ app.use(cors())
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.get('*', (req, res) => { 
+  res.sendFile(path.join(__dirname, 'dist', 'index.html')); 
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
