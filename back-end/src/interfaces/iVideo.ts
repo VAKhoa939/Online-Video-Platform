b8feadff682361b6163978bc2d3e6a997ad58940
Kaos <21110046@student.hcmute.edu.vn>
2024-12-14 00:13:59 +0700

    delete .env

--- back-end/src/interfaces/iVideo.ts
+++ back-end/src/interfaces/iVideo.ts
@@ -1,7 +1,7 @@
 import { Types } from "mongoose";
 
 export default interface IVideo extends Document {
-  _id: Types.ObjectId[];
+  _id: Types.ObjectId;
   videoLink: string;
   thumbnail: string;
   duration: Date;
